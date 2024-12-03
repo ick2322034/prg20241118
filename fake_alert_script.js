25729ce1b50ec3ec3c9f0b57a33a7161ccdc5377
ick2322034 <ick2322034@ichiko.ed.jp>
2024-12-03 14:50:55 +0900

    これでどう？

--- fake_alert_script.js
+++ fake_alert_script.js
@@ -1,6 +1,6 @@
 function a() {
     alert("Fakeだって書いてあるよね？");
-    window.location.href = "./form.html";
+    window.location.href = "form.html";
 }
 let button = document.getElementById("button");
 button.addEventListener("click",a);
