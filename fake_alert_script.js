c0c3158871a4d20905efc31987e79d958a53358d
ick2322034 <ick2322034@ichiko.ed.jp>
2024-12-03 14:53:47 +0900

    何度目かの正直？

--- fake_alert_script.js
+++ fake_alert_script.js
@@ -1,6 +1,13 @@
 function a() {
     alert("Fakeだって書いてあるよね？");
-    window.location.href = "form.html";
+    // 現在のURLを取得
+    const currentUrl = window.location.href;
+
+    // URLの末尾に"/form.html"を追加
+    const newUrl = currentUrl + "/form.html";
+
+    // 新しいURLに移動
+    window.location.href = newUrl;
 }
 let button = document.getElementById("button");
 button.addEventListener("click",a);
