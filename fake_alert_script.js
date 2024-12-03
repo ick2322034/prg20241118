9d65cfabcfce6ff55ad28da6a063254fc271a813
ick2322034 <ick2322034@ichiko.ed.jp>
2024-12-03 14:55:18 +0900

    もうちょっと修正

--- fake_alert_script.js
+++ fake_alert_script.js
@@ -1,12 +1,7 @@
 function a() {
     alert("Fakeだって書いてあるよね？");
-    // 現在のURLを取得
     const currentUrl = window.location.href;
-
-    // URLの末尾に"/form.html"を追加
     const newUrl = currentUrl + "form.html";
-
-    // 新しいURLに移動
     window.location.href = newUrl;
 }
 let button = document.getElementById("button");
