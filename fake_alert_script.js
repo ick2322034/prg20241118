a4ed349cb3de036a8f379785792ab9128058a07d
ick2322034 <ick2322034@ichiko.ed.jp>
2025-01-14 14:12:31 +0900

    いろいろ修正

--- fake_alert_script.js
+++
@@ -1,9 +0,0 @@
-function a() {
-    alert("Fakeだって書いてあるよね？");
-    const currentUrl = window.location.href;
-    const newUrl = currentUrl + "form.html";
-    window.location.href = newUrl;
-}
-let button = document.getElementById("button");
-button.addEventListener("click",a);
-alert("このページはフェイクだよ！")
